2e9763d22a2f7783a5be4e5498253bc7ebe75da9
Jhonghee Park <jhonghee@gmail.com>
2016-08-20 00:19:25 -0400

    added -t Hugo-Octopress

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace by `hugo -t <yourtheme>`
+hugo -t Hugo-Octopress # if using a theme, replace by `hugo -t <yourtheme>`
 
 # Go To Public folder
 cd public
